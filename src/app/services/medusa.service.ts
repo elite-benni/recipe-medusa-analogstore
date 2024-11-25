ce18a7840e69f0743058be183f675f57e5706b30
elite-benni <benjamin.heiss@elite.tirol>
2024-11-25 19:38:46 +0000

    reactivate medusa SDK

--- src/app/services/medusa.service.ts
+++ src/app/services/medusa.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, InjectionToken } from '@angular/core';
-// import Medusa from '@medusajs/js-sdk';
-import{ Config } from '@medusajs/js-sdk';
-import{ StoreProductListResponse } from '@medusajs/types';
-// import { from } from 'rxjs';
+import Medusa, { Config } from '@medusajs/js-sdk';
+import { StoreProductListResponse } from '@medusajs/types';
+import { from } from 'rxjs';
 
 export const MEDUSA_CONFIG = new InjectionToken<Config>('MEDUSA_CONFIG');
 
@@ -11,35 +10,34 @@ export function provideMedusaConfig(config: Config) {
   return {
     provide: MEDUSA_CONFIG,
     useValue: config || {
-      baseUrl: ''
-    }
-  }
+      baseUrl: '',
+    },
+  };
 }
 
 @Injectable({ providedIn: 'root' })
 export class MedusaService {
-  // #sdk: Medusa;
+  #sdk: Medusa;
   #medusaConfig = inject(MEDUSA_CONFIG);
   #http = inject(HttpClient);
 
-  // constructor() {
-  //   this.#sdk = new Medusa({
-  //     debug: import.meta.env['NODE_ENV'] === 'development',
-  //     ...this.#medusaConfig
-  //   });
-  // }
-
-  productList$() {
-    return this.#http.get<StoreProductListResponse>(
-      `${this.#medusaConfig.baseUrl}/store/products`,
-      {
-        headers: {
-          'x-publishable-api-key': this.#medusaConfig.publishableKey || ''
-        }
-      }
-    );
-
-    // return from(this.#sdk.store.product.list());
+  constructor() {
+    this.#sdk = new Medusa({
+      debug: import.meta.env['NODE_ENV'] === 'development',
+      ...this.#medusaConfig,
+    });
   }
 
+  productList$() {
+    // return this.#http.get<StoreProductListResponse>(
+    //   `${this.#medusaConfig.baseUrl}/store/products`,
+    //   {
+    //     headers: {
+    //       'x-publishable-api-key': this.#medusaConfig.publishableKey || '',
+    //     },
+    //   }
+    // );
+
+    return from(this.#sdk.store.product.list());
+  }
 }
